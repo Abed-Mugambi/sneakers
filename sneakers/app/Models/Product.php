129e1fab5790800ed68451a8f59fb595ebd4cea3
Abed <abedmugambi@gmail.com>
2024-04-09 12:36:34 +0300

    Relationships created

--- sneakers/app/Models/Product.php
+++ sneakers/app/Models/Product.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Reviews;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function reviews()
+    {
+        return $this->hasMany(Reviews::class);
+    }
+
+
+
+
 }
