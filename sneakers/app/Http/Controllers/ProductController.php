71949cd11704a30d12c72863ddd6c6604f4d09a1
Abed <abedmugambi@gmail.com>
2024-04-09 14:58:54 +0300

    product collection resource

--- sneakers/app/Http/Controllers/ProductController.php
+++ sneakers/app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Product\ProductCollection;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
@@ -13,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return  ProductCollection::collection(Product::all());
     }
 
     /**
