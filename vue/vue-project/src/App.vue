a25e4be0d8ff8cc222a82c917cd6cd88d5901557
Abed <abedmugambi@gmail.com>
2024-04-15 12:12:53 +0300

    adding cartIcon

--- vue/vue-project/src/App.vue
+++ vue/vue-project/src/App.vue
@@ -1,8 +1,19 @@
+
 <script setup>
+
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { fas } from '@fortawesome/free-solid-svg-icons';
+
+// library.add(fas); 
+// Add all solid icons to the library
+
 </script>
 
+<!-- import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'; -->
 <template>
   <header>
 
@@ -11,20 +22,26 @@ import HelloWorld from './components/HelloWorld.vue'
       <!-- navbar -->
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <div class="container-fluid">
-            <RouterLink class="navbar-brand" to="/">Navbar</RouterLink>
+
+            <RouterLink class="navbar-brand" to="/">Home</RouterLink>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 
-                <li class="nav-item">
-                  <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
-                </li>
-
                 <!-- <li class="nav-item">
-                  <RouterLink class="nav-link active" aria-current="page" to="/about">About</RouterLink>
+                  <RouterLink class="nav-link active" aria-current="page" to="/">Home</RouterLink>
                 </li> -->
+
+                <li class="nav-item">
+                  <RouterLink class="nav-link active" aria-current="page" to="/cart">Cart</RouterLink>
+
+                  <!-- <font-awesome-icon :icon="['fas', 'cart-shopping']" /> -->
+                  <!-- add cart icon here -->
+
+                  <i class="fas fa-search"></i>
+                </li>
 
                 <li class="nav-item">
                   <RouterLink class="nav-link active" aria-current="page" to="/products">Products</RouterLink>
--- vue/vue-project/src/views/HomeView.vue
+++ vue/vue-project/src/views/HomeView.vue
@@ -1,8 +1,125 @@
 
 
-<template>
-  <main>
-    <h1>welcome to home page</h1>
-  </main>
-</template>
 
+
+<template>
+
+    
+
+    <div class="card">
+    
+        <div class="card-header">
+            
+            <!-- <h4>
+                Products
+                <RouterLink to="/products/create"
+                    class="btn btn-primary float-end">
+                    Add Product
+    
+                </RouterLink>
+    
+            </h4> -->
+        </div>
+    
+            <div class="card-body">
+    
+                         
+                              
+                        <div v-for="(product, index) in this.products" :key="index" class="product">
+    
+                            <div class="img"></div>
+                            <!-- <div>ID: {{ product.id }}</div> -->
+                            <div>Name: {{ product.name }}</div>
+                            <div>Description: {{ product.description }}</div>
+                            <div>Price: {{ product.totalPrice }}</div>
+                            <div>Stock: {{ product.stock }}</div>
+                            <div>Discount: {{ product.discount }}</div>
+    
+                            <div class="action-buttons">
+                               
+                                <!-- <RouterLink to="'/edit/' + product.id" 
+                                class="btn btn-success float-end">
+                                Edit
+    
+                                </RouterLink> -->
+    
+                                <button @click="addToCart(product.id)" 
+                                class="btn btn-success">ADD TO CART</button>
+    
+                            </div>
+    
+                        </div>
+              
+            </div>
+    
+    </div>
+        
+    </template>
+    
+    
+    <script>
+    
+    
+    
+    import axios from 'axios';
+    
+    
+    export default  {
+        name: 'products',
+        data(){
+            return {
+                products: []
+            }
+        },
+    
+        mounted(){
+            this.getProducts();
+    
+    
+        },
+    
+        methods: {
+            getProducts() {
+    
+              axios.get('http://localhost:8000/api/products').then(res=> {
+               this.products = res.data.data
+    
+                console.log(this.products)
+              });
+            }
+        },
+    }
+    </script>
+    
+    <style>
+    
+    .img{
+        border: 1px solid black;
+        height: 200px;
+        object-fit: cover;
+    }
+    
+    .card-body {
+            border: 1px solid #ddd; /* Add border to card-body */
+            padding: 10px; /* Add padding for better spacing */
+            margin-bottom: 10px; /* Add margin between card-body and card-footer */
+            display: flex;
+            flex-direction: row;
+             /* Set a maximum height */
+        overflow-y: auto; /* Enable vertical scrolling */
+        }
+    
+        .product {
+            border: 1px solid #ddd; /* Add border to product cards */
+            padding: 10px; /* Add padding for better spacing */
+            margin-bottom: 10px; /* Add margin between product cards */
+            height: 430px;
+            width: 360px;
+        }
+    
+        .action-buttons {
+            margin-top: 10px; /* Add margin above action buttons */
+        }
+    
+    
+    </style>
